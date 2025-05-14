d09f9256d689475295bae649d0f578e00e1eec9b
chxtz2077 <caixiangyang4@gmail.com>
2025-05-14 14:26:30 +0800

    Site updated: 2025-05-14 14:26:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/06/hello-world/"];function toRandomPost(){
+var posts=["2025/05/06/hello-world/","2025/05/14/notion_articles/测试的文章-2025-05-14/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
